8a93a64e3273f23d06cb51b9f164b4fe74903db8
edwardndiyo <ndiyoedward@gmail.com>
2023-06-16 20:19:04 +0100

    second commit

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName: 'Mastering Entreis course',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        started: 'Progress'
+    },
+    {
+        productName: 'Beginner to pro course',
+        productNumber: '66578' ,
+        paymentStatus: 'Refunded',
+        started: 'Progress'
+    },
+    {
+        productName: 'Signal Services',
+        productNumber: '76435',
+        paymentStatus: 'Not started',
+        started: 'expires in one week'
+    },
+    {
+        productName: 'copytrading',
+        productNumber: '55432',
+        paymentStatus: 'funded $700',
+        started: 'expired'
+    },
+    {
+        productName: 'Mastering Entreis course',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        started: 'Progress'
+    },
+    {
+        productName: 'Beginner to pro course',
+        productNumber: '66578' ,
+        paymentStatus: 'Refunded',
+        started: 'Progress'
+    },
+    {
+        productName: 'Signal Services',
+        productNumber: '76435',
+        paymentStatus: 'Not started',
+        started: 'expires in one week'
+    },
+    {
+        productName: 'copytrading',
+        productNumber: '55432',
+        paymentStatus: 'funded $700',
+        started: '5th june'
+    }
+]
